6d41685957f8825ce0449828f5af77c319cc3708
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2009-03-07 17:03:07 +0000

    CHanged th way we find path

--- tools/make_core_modules_pkg.pl
+++ tools/make_core_modules_pkg.pl
@@ -3,7 +3,7 @@
 use strict;
 use Archive::Zip qw/:ERROR_CODES :CONSTANTS/;;
 use Module::CoreList;
-use File::Basename;
+use Cwd;
 
 my @core_mods = keys %{$Module::CoreList::version{5.01}};
 my @ldirs = qw,auto arm-linux-multi arm-linux-multi/auto,;
@@ -11,7 +11,7 @@ my @ldirs = qw,auto arm-linux-multi arm-linux-multi/auto,;
 my $mods_dir;
 $mods_dir = shift and -d $mods_dir or usage();
 usage() if @ARGV;
-my $target = dirname($0) . "/target";
+my $target = getcwd . "/target";
 
 die "$target already exists, erase it before..." if -e $target;
 
