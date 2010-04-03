850b2a260544acb60d929bc4ca743220072f79f0
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-04-03 00:28:01 +0000

    beginning of building in steps

---
+++ trunk/tools/01_make_bindings.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd ~/perldroid/tools
+
+rm -rf target_droid
+wget -nd -O public_api.xml "http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob_plain;f=api/current.xml;hb=cupcake"
+
+./make_bindings.pl public_api.xml
