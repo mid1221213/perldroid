7bc89094fc8a9d6cae0515d917fcda92abe2e1a9
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-04-04 03:17:48 +0000

    deleted old content

--- tools/00_clean.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-rm -rf target_droid public_api.xml proxy_classes.list java2jni.pl target_core
--- tools/01_make_bindings.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm -rf target_droid
-wget -nd -O public_api.xml "http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob_plain;f=api/current.xml;hb=cupcake"
-
-./make_bindings.pl public_api.xml
