9e43c048b08cc0e81dd1bbd1f09a21bf1efb32d5
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-04-04 04:18:51 +0000

    1st compilation OK of libPerlDroid.so

--- project/jni/Android.mk
+++ project/jni/Android.mk
@@ -1,9 +1,11 @@
 LOCAL_PATH := $(call my-dir)
-DUMMY := $(shell cd $(LOCAL_PATH); make -f libperl.mk)
+DUMMY := $(shell cd $(LOCAL_PATH); make -f libperl.mk 1>&2)
 
 include $(CLEAR_VARS)
 
+LOCAL_CFLAGS    := -I$(LOCAL_PATH)/libperl/perl-5.10.0
+LOCAL_LDLIBS    := -llog -ldl
 LOCAL_MODULE    := PerlDroid
-LOCAL_SRC_FILES := libPerlDroid.c
+LOCAL_SRC_FILES := libPerlDroid.c JNIHelp.c
 
 include $(BUILD_SHARED_LIBRARY)
