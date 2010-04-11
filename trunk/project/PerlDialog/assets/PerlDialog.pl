6f7a85a2bd4c08cbb930954c92747de470bcd006
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-04-11 13:09:48 +0000

    added pkg parameter to XS_proxy

--- trunk/project/PerlDialog/assets/PerlDialog.pl
+++ trunk/project/PerlDialog/assets/PerlDialog.pl
@@ -9,19 +9,7 @@ use PerlDroid::android::widget;
 
 use vars qw/$this/;
 
-sub onClick {
-    my ($arg1, $arg2) = @_;
-    print "arg1=$arg1, arg2=$arg2\n";
-    if ($arg2 == - 1) {
-	warn 'class1 = ' . $arg1->getClass->getName;
-	$arg1->cancel;
-    } else {
-	warn 'classthis = ' . $this->getClass->getName;
-	$this->finish;
-    }
-}
-
-my $pm = PerlDroid::XS_proxy($DialogInterface_OnClickListener);
+my $pm = PerlDroid::XS_proxy($DialogInterface_OnClickListener, "PerlDialog::DialogInterface");
 
 my $adb = $AlertDialog_Builder->new($this); 
 $adb->setMessage('Salut ma poule !'); 
@@ -34,3 +22,17 @@ print "Ok\n";
 warn 'classthis = ' . $this->getClass->getName;
 printf("R.layout.main=0x%x\n", $R{layout}{main});
 $this->findViewById($R{id}{TV})->cast($TextView)->setText("Mid was here! :-)");
+
+package PerlDialog::DialogInterface;
+
+sub onClick {
+    my ($arg1, $arg2) = @_;
+    print "arg1=$arg1, arg2=$arg2\n";
+    if ($arg2 == - 1) {
+	warn 'class1 = ' . $arg1->getClass->getName;
+	$arg1->cancel;
+    } else {
+	warn 'classthis = ' . $main::this->getClass->getName;
+	$main::this->finish;
+    }
+}
