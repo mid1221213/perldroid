705122498d69e0309d3eda9d349f8de89e57b7da
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-04-19 11:50:04 +0000

    changed to make in NDK

--- project/tools/make_scp_all.sh
+++ project/tools/make_scp_all.sh
@@ -6,11 +6,11 @@ rm -rf target_core PerlDroid.zip
 
 ./make_core_modules_pkg.pl ~/android-ndk/apps/perldroid/project/jni/libperl/perl-5.10.0/install_me_here/usr/lib/perl/5.10.0
 
-mkdir target_droid
+mkdir -p target_droid
 cd target_droid
 mkdir -p arm-linux-multi/auto/PerlDroid
-cd ../../PerlDroid/jni/
-make
+cd ~/android-ndk
+make APP=perldroid
 cd -
 mv ~/android-ndk/apps/perldroid/project/libs/armeabi/libPerlDroid_so.so arm-linux-multi/auto/PerlDroid/PerlDroid.so
 find . -name "*.*" | zip ../target_core/PerlDroid.zip -@
