54d63e5402f775b478d1244fdc844af5627818a1
brouits <brouits@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-05-15 22:04:04 +0000

    added application table into db

--- project/PerlDroid/src/org/gtmp/perl/PerlDroidDb.java
+++ project/PerlDroid/src/org/gtmp/perl/PerlDroidDb.java
@@ -16,6 +16,14 @@ public class PerlDroidDb {
     private static final String DATABASE_NAME = "perldroid";
     private static final int DATABASE_VERSION = 1;
 
+    public static final String APP_TABLE = "application";
+    public static final String KEY_APPID = "_id"; /* important for magics with adapter */
+    public static final String KEY_APPNAME = "name";
+
+    public static final String APPMOD_TABLE = "app_mod";
+    public static final String APPMOD_FKEY_APPID = "appid";
+    public static final String APPMOD_FKEY_MODID = "modid";
+
     public static final String MODULE_TABLE = "module";
     public static final String KEY_MODID = "_id"; /* important for magics with adapter */
     public static final String KEY_MODNAME = "name";
@@ -27,6 +35,16 @@ public class PerlDroidDb {
     private DatabaseHelper mDbHelper;
     private SQLiteDatabase mDb;
     
+    private static final String APP_TABLE_CREATE =
+	"create table " + APP_TABLE + " (" 
+	+ KEY_APPID + " integer primary key autoincrement, " 
+	+ KEY_APPNAME + " text not null);";
+
+    private static final String APPMOD_TABLE_CREATE =
+	"create table " + APPMOD_TABLE + " (" 
+	+ APPMOD_FKEY_APPID + " integer not null, " 
+	+ APPMOD_FKEY_MODID + " integer not null);";
+
     private static final String MODULE_TABLE_CREATE =
 	"create table " + MODULE_TABLE + " (" 
 	+ KEY_MODID + " integer primary key autoincrement, " 
@@ -48,6 +66,8 @@ public class PerlDroidDb {
         @Override
 	    public void onCreate(SQLiteDatabase db) {
 
+            db.execSQL(APP_TABLE_CREATE);
+            db.execSQL(APPMOD_TABLE_CREATE);
             db.execSQL(MODULE_TABLE_CREATE);
             db.execSQL(FILES_TABLE_CREATE);
         }
@@ -56,6 +76,8 @@ public class PerlDroidDb {
 	    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
 		  + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + APP_TABLE);
+            db.execSQL("DROP TABLE IF EXISTS " + APPMOD_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + MODULE_TABLE);
             db.execSQL("DROP TABLE IF EXISTS " + FILES_TABLE);
             onCreate(db);
@@ -91,6 +113,60 @@ public class PerlDroidDb {
         mDbHelper.close();
     }
 
+
+    /**
+     * Create a new application entry using the infos provided. If the application is
+     * successfully created return the new rowId for that app, otherwise return
+     * a -1 to indicate failure.
+     */
+    public long createApplication(String app, ArrayList<String> modules) {
+        ContentValues appValue = new ContentValues();
+        appValue.put(KEY_APPNAME, app);
+        long appId =  mDb.replace(APP_TABLE, null, appValue);
+
+        for (String module: modules) {
+            Cursor cur = getModId(module);
+            cur.moveToFirst();
+            long modId = cur.getLong(cur.getColumnIndex(KEY_MODID));
+            cur.close();
+            cur = null;
+            ContentValues appmodValue = new ContentValues ();
+            appmodValue.put(APPMOD_FKEY_APPID, appId);
+            appmodValue.put(APPMOD_FKEY_MODID, modId);
+            long joinId = mDb.insert(APPMOD_TABLE, null, appmodValue);
+            if (joinId == -1)
+                return joinId;
+        }
+        return appId;
+    }
+
+    /** 
+     * Return an application id given it name
+     */
+    public Cursor getAppId(String app) {
+        return  mDb.query(APP_TABLE, new String[] {KEY_APPID},
+                          KEY_APPNAME + "='" + app + "'", null, null, null, null);
+    }
+
+    /**
+     * Delete the application with the given appId
+     */
+    public boolean deleteApplication(long appId) {
+        boolean ok = true;
+        ok &= mDb.delete(APPMOD_TABLE, APPMOD_FKEY_APPID + "='" + Long.toString(appId) + "'", null) > 0;
+        ok &= mDb.delete(APP_TABLE, KEY_APPID + "='" + Long.toString(appId) + "'", null) > 0;
+
+        Cursor cur = mDb.rawQuery("SELECT " + KEY_MODID + " FROM " + MODULE_TABLE + " LEFT JOIN " + APPMOD_TABLE + " ON " + KEY_MODID + "=" + APPMOD_FKEY_MODID + " WHERE " + APPMOD_FKEY_MODID + "is NULL", null);
+        cur.moveToFirst();
+        while (!cur.isAfterLast()) {
+            long modId = cur.getLong(cur.getColumnIndex(KEY_MODID));
+            ok &= deleteModule(modId);
+	    cur.moveToNext();
+        }
+        cur.close();
+        cur = null;
+        return ok;
+    }
 
     /**
      * Create a new module entry using the infos provided. If the module is
