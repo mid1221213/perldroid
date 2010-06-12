7df882337e60c5fc4684cb9c017170a37051417b
feckmicrosoft <feckmicrosoft@78b0b5ca-e9ab-11dd-b2d9-215335d0b316>
2010-06-12 02:50:11 +0000

    added rm of _so.so

--- tools/02_make.sh
+++ tools/02_make.sh
@@ -6,4 +6,9 @@ make -f libperl.mk
 
 # Make the rest
 ~/android-ndk/ndk-build
+
+# Copy libperl.so to libs
 cp -v libperl/perl-5.10.1/libperl.so ../libs/armeabi/
+
+# Remove libPerlDroid_so.so (not needed for PerlDroid.apk)
+rm -f ../libs/armeabi/libPerlDroid_so.so
