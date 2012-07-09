2e321c72c8b9f923bb54a7e18aa31c2258fff8fb
feckmicrosoft <feckmicrosoft@gmail.com>
2012-07-09 13:54:41 +0000

    more cpan building. should be OK for EUMM modules

---
+++ libperl/perl-5.16.0/Cross/prepare_perl_for_cpan.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+VERSION=5.16.0
+
+if [[ ! -d ~/perl-$VERSION/install_me_here || -d ~/.cpan ]]; then
+    echo "You don't want to do that. See README.android"
+    cat README.android 2>/dev/null
+    exit 0
+fi
+
+mkdir -p ~/.cpan/CPAN
+cp MyConfig.pm ~/.cpan/CPAN/
+
+cd ~/perl-$VERSION/
+cp ~/perldroid/libperl/perl-$VERSION/Cross/cpan.android .
+
+cp -a install_me_here/lib/* fake_config_library/
+
+echo now do:
+echo cd ~/perl-$VERSION/
+echo ./cpan.android MODULE::TO::INSTALL ...
