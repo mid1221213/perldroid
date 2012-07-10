5c7f266f66cc5b7705f8af04cca8f4d05b01a19a
feckmicrosoft <feckmicrosoft@gmail.com>
2012-07-10 02:25:57 +0000

    bugfixes

--- libperl/perl-5.16.0/Cross/prepare_perl_for_cpan.sh
+++ libperl/perl-5.16.0/Cross/prepare_perl_for_cpan.sh
@@ -2,22 +2,24 @@
 
 VERSION=5.16.0
 
-if [[ ! -d ~/perl-$VERSION/install_me_here || -e ~/perl-$VERSION/install_me_here/strict.pm || -d ~/.cpan ]]; then
+if [[ ! -d ~/perl-$VERSION/install_me_here || -e ~/perl-$VERSION/fake_config_library/strict.pm || -d ~/.cpan.android ]]; then
     echo "You don't want to do that. See README.android"
     cat README.android 2>/dev/null
     exit 0
 fi
 
-mkdir -p ~/.cpan/CPAN
-cp MyConfig.pm ~/.cpan/CPAN/
+mkdir -p ~/.cpan.android/CPAN
+cp MyConfig.pm ~/.cpan.android/CPAN/
 
 cd ~/perl-$VERSION/
 cp ~/perldroid/libperl/perl-$VERSION/Cross/cpan.android .
 
 mv perl perl.arm
 ln -s miniperl perl
-cp -a install_me_here/lib/* fake_config_library/
+mv fake_config_library/Config.pm fake_config_library/Config_heavy.pl install_me_here/lib/
+rmdir fake_config_library && ln -s install_me_here/lib/ fake_config_library
 
-echo now do:
+echo Now do:
 echo cd ~/perl-$VERSION/
+echo PATH=\$PATH:~/perl-$VERSION/
 echo ./cpan.android MODULE::TO::INSTALL ...
